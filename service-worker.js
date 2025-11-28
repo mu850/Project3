6883ad846ce64b2c68d7e49261a030ec1cb709b1
mu850 <mu7474674@gmail.com>
2025-11-28 07:34:21 +0500

    Implement service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open("v1").then(cache => {
+            return cache.addAll([
+                "./",
+                "index.html",
+                "style.css",
+                "script.js",
+                "manifest.json"
+            ]);
+        })
+    );
+});
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(resp => resp || fetch(e.request))
+    );
+});
